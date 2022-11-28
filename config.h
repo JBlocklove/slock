2568f4597ea6882d77e82277bd92e69e472d16d5
Jason Blocklove <jason.blocklove@gmail.com>
2022-11-28 12:05:54 -0500

    Trying to add PAM authentication that will work with pam-gnupg

--- config.h
+++ config.h
@@ -1,12 +1,16 @@
 /* user and group to drop privileges to */
 static const char *user  = "jason";
-static const char *group = "users";
+static const char *group = "jason";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "#595959",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
+	[PAM] =    "#9400D3",   /* waiting for PAM */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* PAM service that's used for authentication */
+static const char* pam_service = "login";
